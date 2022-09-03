3ab084db46bd5cb70a3c671b8589fbfa7b5a04c9
Mariluz08 <mariluz14955@gmail.com>
2022-09-02 23:15:11 -0500

    Ajustes finales proyecto final

--- projecto_nodejs/projecto_nodejs/routes/api.js
+++ projecto_nodejs/projecto_nodejs/routes/api.js
@@ -11,6 +11,9 @@ let AuthController = require('../controllers/authController');
 let userProtectUrl = require('../middlewares/authUser').userProtectUrl;
 
 api.get("/", WelcomeController.welcome);
+
+// --- Alumno -----------------
+
 api.get("/alumnos", AlumnosController.alumnos);
 api.get("/alumno/:n_lista", AlumnosController.alumno);
 api.post("/alumno", userProtectUrl, [
@@ -28,6 +31,26 @@ api.put("/alumno/:n_lista", [
 
 api.delete("/alumno/:n_lista", AlumnosController.delete_alumno);
 
+//------ Maestros --------------
+
+api.get("/maestros", MaestrosController.maestros);
+api.get("/maestro/:n_lista", MaestrosController.maestro);
+api.post("/maestro", userProtectUrl, [
+    body('n_lista').not().isEmpty(),
+    body('nombre').not().isEmpty(),
+    body('edad').not().isEmpty(),
+    body('genero').not().isEmpty()
+], MaestrosController.crear_maestro);
+
+api.put("/maestro/:n_lista", [
+    body('nombre').not().isEmpty(),
+    body('edad').not().isEmpty(),
+    body('genero').not().isEmpty()
+], MaestrosController.update_maestro);
+
+api.delete("/maestro/:n_lista", MaestrosController.delete_maestro);
+
+//----- Login
 api.post("/login", [
     body('mail').not().isEmpty(),
     body('pass').not().isEmpty()
